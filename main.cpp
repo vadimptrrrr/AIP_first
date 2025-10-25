0e2b5e2aeaa798d04bb90004da1c74812394c69c
Petrov Vadim <vadikdydy@mail.ru>
2025-10-25 12:36:24 +0300

    input one unsigned int

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+#incklude <iostream>
+
 int main(){
-  
+  using u_t = unsigned;
+  u_t a = 0;
+  std::cin >> a;
+
+  if (std::cin.eoF()){
+    std::cout << 0 << "\n";
+  }elseif(std::cin.fail(){
+    std::cerr << "Eror\n";
+    return 1;
+  }
+
+  return 0;
 }
