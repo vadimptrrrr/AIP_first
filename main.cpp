cfa924090b62eb03af3d0021a217b7a8d54c4ad4
Petrov Vadim <vadikdydy@mail.ru>
2025-10-25 12:48:42 +0300

    not fine

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
-#incklude <iostream>
+#include <iostream>
+#include <cstddef>
 
 int main(){
   using u_t = unsigned;
   u_t a = 0;
-  std::cin >> a;
+  size_t count = 0;
 
-  if (std::cin.eoF()){
-    std::cout << 0 << "\n";
-  }elseif(std::cin.fail(){
+  std::cin >> a;
+  
+  while (std::cin >> a){
+    count++;
+  }
+
+  if (std::cin.eof()){
+    std::cout << count << "\n";
+  }else if(std::cin.fail()){
     std::cerr << "Eror\n";
     return 1;
   }
